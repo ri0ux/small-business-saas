2f3255d9db2df21955eef8754689ce63cf8e491e
Max <zimmermaxwell@gmail.com>
2025-12-08 21:00:43 -0600

    Auth set up

---
+++ app-web/lib/client.ts
@@ -0,0 +1,10 @@
+import { createBrowserClient } from "@supabase/ssr";
+
+export function createClient() {
+  console.log("hiiiiiiiiiii", process.env.NEXT_PUBLIC_SUPABASE_URL);
+
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+  );
+}
